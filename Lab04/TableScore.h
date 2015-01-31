9c3d4efb7f042aa011fcdb70160a45fd7ff84e77
jvlioverne <jvlioverne@gmail.com>
2015-01-31 11:46:22 -0600

    primera parte

---
+++ Lab04/TableScore.h
@@ -0,0 +1,15 @@
+//
+//  TableScore.h
+//  Lab04
+//
+//  Created by Benjas on 29/01/15.
+//  Copyright (c) 2015 Benjas. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TableScore : UIViewController <UITableViewDelegate, UITableViewDataSource, UIAlertViewDelegate>
+
+@property (strong, nonatomic) IBOutlet UITableView *tblScore;
+
+@end
